f57dc6d1ba266a79fc96a915411bf9c4a173a568
Leenard <lolleenard0@gmail.com>
2024-06-15 22:04:20 +0800

    feat: protected routes

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,14 +1,43 @@
 import { getToken } from "next-auth/jwt";
 import { withAuth } from "next-auth/middleware";
+import { NextResponse } from "next/server";
 
-export default withAuth(async function middleware(req, res) {
-  const pathname = req.nextUrl.pathname;
+export default withAuth(
+  async function middleware(req, res) {
+    const pathname = req.nextUrl.pathname;
 
-  const isAuth = getToken({ req });
-  const userInLogin = pathname.startsWith("/login");
-  const sensitiveRoutes = ["/dashboard"];
-});
+    const isAuth = await getToken({ req });
+    const userInLogin = pathname.startsWith("/login");
+    const sensitiveRoutes = ["/feed"];
+    const userInSensitiveRoute = sensitiveRoutes.some((route) =>
+      pathname.startsWith(route)
+    );
+
+    if (userInLogin) {
+      if (isAuth) {
+        return NextResponse.redirect(new URL("/feed", req.url));
+      }
+
+      return NextResponse.next();
+    }
+
+    if (!isAuth && userInSensitiveRoute) {
+      return NextResponse.redirect(new URL("/login", req.url));
+    }
+
+    if (pathname === "/") {
+      return NextResponse.redirect(new URL("/feed", req.url));
+    }
+  },
+  {
+    callbacks: {
+      async authorized() {
+        return true;
+      },
+    },
+  }
+);
 
 export const config = {
-  matchter: ["/", "/login", "/dashboard/:path*"],
+  matchter: ["/", "/login", "/feed/:path*"],
 };
