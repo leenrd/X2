eb65fd8557438e4101a796998a73ee954b44c1c6
Leenard <lolleenard0@gmail.com>
2024-06-15 21:55:47 +0800

    doc: update readme

--- README.md
+++ README.md
@@ -1,3 +1,10 @@
 # X2
 
 real-time chat application keke
+
+feat:
+
+- refactor ui
+- rooms?
+- dark/light mode
+- ai translate?
--- src/middleware.ts
+++ src/middleware.ts
@@ -1,7 +1,12 @@
+import { getToken } from "next-auth/jwt";
 import { withAuth } from "next-auth/middleware";
 
 export default withAuth(async function middleware(req, res) {
-  // Your code here
+  const pathname = req.nextUrl.pathname;
+
+  const isAuth = getToken({ req });
+  const userInLogin = pathname.startsWith("/login");
+  const sensitiveRoutes = ["/dashboard"];
 });
 
 export const config = {
